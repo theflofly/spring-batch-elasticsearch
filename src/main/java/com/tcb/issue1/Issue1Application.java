448edb7ba59c0e439a02551c48c9ac70e441d665
Florian Courtial <florian.courtial@sicap.com>
2015-12-28 14:03:50 +0100

    First commit, empty project.

---
+++ src/main/java/com/tcb/issue1/Issue1Application.java
@@ -0,0 +1,12 @@
+package com.tcb.issue1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issue1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Issue1Application.class, args);
+    }
+}
---
+++ src/test/java/com/tcb/issue1/Issue1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tcb.issue1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Issue1Application.class)
+public class Issue1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
