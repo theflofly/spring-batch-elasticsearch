2659bebb252f108edc0f0f9fa9b59a9398ed7212
Florian Courtial <floriancourtial@gmail.com>
2016-02-11 08:03:07 +0100

    Adding the offer writer.

--- src/main/java/com/tcb/issue1/job/OfferItemWriter.java
+++ src/main/java/com/tcb/issue1/job/OfferItemWriter.java
@@ -1,7 +1,29 @@
 package com.tcb.issue1.job;
 
+import com.tcb.issue1.model.Offer;
+import com.tcb.issue1.repository.OfferRepository;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 /**
- * Created by fco on 28/01/16.
+ * This class is the writer for the Offer object, it writes a list of Offers into the Elasticsearch database.
+ *
+ * Created by Florian.Courtial on 28/01/16.
  */
-public class OfferItemWriter {
+public class OfferItemWriter implements ItemWriter<Offer>, InitializingBean {
+
+    @Autowired
+    private OfferRepository offerRepository;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {}
+
+    @Override
+    public void write(List<? extends Offer> list) throws Exception {
+        offerRepository.save(list);
+    }
+
 }
